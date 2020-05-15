024b66c7ba9824e9b3c8e8bcf5ce9722f1a73033
Cedric Travelletti <cedrictravelletti@gmail.com>
2020-05-15 13:42:54 +0200

    Can now specify title to plotting functions.

--- MESLAS/meslas/plotting.py
+++ MESLAS/meslas/plotting.py
@@ -8,7 +8,7 @@ import matplotlib as mpl
 from mpl_toolkits.axes_grid1 import ImageGrid
 
 
-def plot_2d_slice(sliced_sample):
+def plot_2d_slice(sliced_sample, title=None):
     """ Plots 2 dimensional slice.
 
     For each of the n_out responses, plot a two dimensional image.
@@ -24,12 +24,16 @@ def plot_2d_slice(sliced_sample):
 
     # Number of responses.
     # Special case if only one, since do not need grid of plots.
-    if len(sliced_sample.shape) <= 3:
-        plt.title(r"$Z^1$")
+    if len(sliced_sample.shape) <= 2:
+        if title is None:
+            title = r"$Z^1$"
+        plt.title(title)
         im = plt.imshow(
                 sliced_sample[:, :].numpy(),
                 vmin=0.8*sample_min, vmax=0.8*sample_max,
-                extent=[0,1,0,1], cmap='plasma')
+                origin="lower",
+                extent=[0,1,0,1],
+                cmap='plasma')
         plt.colorbar(im)
         # plt.toggle_label(True)
         plt.show()
@@ -51,7 +55,9 @@ def plot_2d_slice(sliced_sample):
         im = plot_grid[i].imshow(
                 sliced_sample[:, :, i].numpy(),
                 vmin=0.8*sample_min, vmax=0.8*sample_max,
-                extent=[0,1,0,1], cmap='plasma')
+                origin="lower",
+                extent=[0,1,0,1],
+                cmap='plasma')
 
     # Hide the unused plots.
     for i in range(n_out, len(plot_grid)):
@@ -67,16 +73,33 @@ def plot_krig_slice(sliced_sample, S_y, L_y):
     """ TEMP: plot kriging mean with observations.
     
     """
+    sample_min = torch.min(sliced_sample).item()
+    sample_max = torch.max(sliced_sample).item()
+
     # Number of responses.
+    # Special case if only one, since do not need grid of plots.
+    if len(sliced_sample.shape) <= 2:
+        plt.title(r"$Z^1$")
+        im = plt.imshow(
+                sliced_sample[:, :].numpy(),
+                vmin=0.8*sample_min, vmax=0.8*sample_max,
+                origin="lower",
+                extent=[0,1,0,1],
+                cmap='plasma')
+        # Add the location of the measurement points on top.
+        locs = S_y[L_y == i].numpy()
+        plt.scatter(locs[:, 1], locs[:, 0], marker="x", s=1.5, color="red")
+        plt.colorbar(im)
+        # plt.toggle_label(True)
+        plt.show()
+        return
+
     n_out = sliced_sample.shape[-1]
 
     # Dimensions of the plotting area.
     n_col = int(np.ceil(np.sqrt(n_out)))
     
     n_row = int(n_out/n_col)
-
-    sample_min = torch.min(sliced_sample).item()
-    sample_max = torch.max(sliced_sample).item()
 
     fig = plt.figure()
     plot_grid = ImageGrid(fig, 111, nrows_ncols=(n_row, n_col),
