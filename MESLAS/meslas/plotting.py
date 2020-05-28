9bd4c0629da29b3a04f5cb6843881d1aec5ca04e
Cedric Travelletti <cedrictravelletti@gmail.com>
2020-05-28 11:19:28 +0200

    Definitive plotting function available now. WORKS LIKE A CHARM.

--- MESLAS/meslas/plotting.py
+++ MESLAS/meslas/plotting.py
@@ -198,3 +198,99 @@ def plot_2D_triangular_grid(grid_coords, grid_vals, S_y=None, L_y=None):
         plt.yticks([0.2, 0.4, 0.6, 0.8])
         plt.show()
         return
+
+def plot_grid_values(grid, vals, S_y=None, L_y=None):
+    """ Plot values defined on a grid. Values can me multidimensional
+    responses. In this case, one plot per response will be produced.
+    One can also provide a generalized location vector, those will be added as
+    points on top of the corresponding plot (can be used to plot observation
+    locations in kriging for example).
+
+    Parameters
+    ----------
+    grid: IrregularGrid
+        Grid on which the values are defined.
+    vals: (grid.n_points, p)
+        Values to plot, defined at the grid nodes. The number of response p can
+        be arbitrary.
+    S_y: (n, grid.dim) Tensor (optional)
+        Vector of spatial locations to plot as points.
+    L_y: (n) Tensor (optional)
+        Vector of response indices. Will specify on which plot the points will
+        be added.
+
+    """
+    # Special case if only one, since do not need grid of plots.
+    if len(vals.shape) <= 1:
+        # Interpolate to regular grid
+        reshaped_vals = grid.interpolate_to_image(vals)
+
+        plt.title(r"$Z^1$")
+        im = plt.imshow(
+                reshaped_vals[:, :].numpy(),
+                origin="lower",
+                extent=[0,1,0,1],
+                cmap=cmap)
+
+        if (S_y is not None) and (L_y is not None):
+            # Add the location of the measurement points on top.
+            locs = S_y[L_y == 0].numpy()
+            plt.scatter(locs[:, 1], locs[:, 0], marker="x", s=1.5, color="red")
+
+        plt.xlim([0, 1])
+        plt.ylim([0, 1])
+        plot_grid[i].set_ylim([0, 1])
+        cbar = plt.colorbar(im)
+        cbar.ax.tick_params(labelsize=6)
+        ptl.xticks([0.2, 0.4, 0.6, 0.8])
+        plt.yticks([0.2, 0.4, 0.6, 0.8])
+        plt.show()
+        return
+
+    else: 
+        n_out = vals.shape[-1]
+
+        # Dimensions of the plotting area.
+        n_col = int(np.ceil(np.sqrt(n_out)))
+        n_row = int(n_out/n_col)
+    
+        # Generate the plot array
+        fig = plt.figure()
+        plot_grid = ImageGrid(fig, 111, nrows_ncols=(n_row, n_col),
+                axes_pad=0.45, share_all=True,
+                cbar_location="right", cbar_mode="each", cbar_size="7%", cbar_pad=0.15,)
+    
+        for i in range(n_out):
+            reshaped_vals = grid.interpolate_to_image(vals[:, i])
+            plot_grid[i].set_title(r"$Z^" + str(i+1) + "$")
+            im = plot_grid[i].imshow(
+                    reshaped_vals[:, :].numpy(),
+                    origin="lower",
+                    extent=[0,1,0,1],
+                    cmap=cmap)
+            cax = plot_grid.cbar_axes[i]
+            cbar = cax.colorbar(im)
+            cbar.ax.tick_params(labelsize=8)
+    
+            if (S_y is not None) and (L_y is not None):
+                # Add the location of the measurement points on top.
+                locs = S_y[L_y == i].numpy()
+                plot_grid[i].scatter(locs[:, 1], locs[:, 0], marker="x", s=1.5, color="red")
+
+            # Restore plot borders, which might be deformed by the scatter.
+            plot_grid[i].set_xlim([0, 1])
+            plot_grid[i].set_ylim([0, 1])
+    
+        # Hide the unused plots.
+        for i in range(n_out, len(plot_grid)):
+            plot_grid[i].axis("off")
+    
+        # Colorbar
+        ax = plot_grid[i]
+        ax.cax.colorbar(im)
+        ax.cax.toggle_label(True)
+    
+        plot_grid.axes_llc.set_xticks([0.2, 0.4, 0.6, 0.8])
+        plot_grid.axes_llc.set_yticks([0.2, 0.4, 0.6, 0.8])
+    
+        plt.show()
